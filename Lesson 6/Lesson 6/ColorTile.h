c05a21be89d0c93ca802424cef67cf3cef043452
Kosenkov <9588434@gmail.com>
2023-11-02 20:26:15 +0300

    Lesson 6

---
+++ Lesson 6/Lesson 6/ColorTile.h
@@ -0,0 +1,16 @@
+//
+//  ColorTile.h
+//  Lesson 6
+//
+//  Created by Ivan Kosenkov on 02.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ColorTile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lesson 6/Lesson 6/ColorTile.m
@@ -0,0 +1,12 @@
+//
+//  ColorTile.m
+//  Lesson 6
+//
+//  Created by Ivan Kosenkov on 02.11.2023.
+//
+
+#import "ColorTile.h"
+
+@implementation ColorTile
+
+@end
